3e2e68898f2f16ebdd725313d77dfff738f27474
Thekips <issizhe@gmail.com>
2022-01-25 10:47:48 +0800

    change .vimrc to _vimrc

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
 rm $HOME/.vimrc
-cp $HOME/vim/vimrc $HOME/.vimrc
+cp $HOME/vim/vimrc $HOME/_vimrc
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni $HOME/vimfiles/autoload/plug.vim -Force
