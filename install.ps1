1521799de02e12ccc81150795d441ddfc2a00c84
Thekips <issizhe@gmail.com>
2022-02-25 14:03:19 +0800

    Update install.ps1
    add test process before delete file

--- install.ps1
+++ install.ps1
@@ -1,4 +1,11 @@
-rm $HOME/.vimrc
+if (Test-Path $HOME/_vimrc) {
+    Remove-Item $HOME/_vimrc -Force
+}
+
 cp $HOME/vim/vimrc $HOME/_vimrc
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni $HOME/vimfiles/autoload/plug.vim -Force
+
+if (Test-Path $HOME/vim) {
+    Remove-Item $HOME/vim -Recurse -Force
+}
