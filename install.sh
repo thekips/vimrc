532e4e457d974b61f63f849b9def9fa42f385df6
Thekips <issizhe@gmail.com>
2022-03-11 11:31:30 +0800

    add delete function.

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-cp vimrc ~/.vimrc
+cp ~/vim/vimrc ~/.vimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+rm -rf ~/vim
